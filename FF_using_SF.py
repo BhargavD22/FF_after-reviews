1e212d2ed1cacc33addf24eece327e2bdec04675
BhargavD22 <bdoddi.miraclesoft@gmail.com>
2025-09-17 20:37:40 +0530

    Update FF_using_SF.py

--- FF_using_SF.py
+++ FF_using_SF.py
@@ -137,7 +137,7 @@ st.markdown(
 )
 
 # --- App header + guide ---
-st.title("📈 Financial Forecasting Dashboard(Revenue)")
+st.title("📈 Financial Forecasting- Revenue Dashboard")
 
 # --- Sidebar controls (sticky) ---
 with st.sidebar:
